ee5e1e226206b1faa4c02f64108c9ff8d50423f2
ramity <lcb005@email.uark.edu>
2017-02-01 21:38:31 -0600

    Added simple gui to showcase actions

--- main.cpp
+++ main.cpp
@@ -4,11 +4,52 @@ int main()
 {
     Table table;
     table.read("people.txt");
-    table.print();
 
-    table.searchFirstName("Chuck");
+    while(true)
+    {
+        int choice = 0;
+        string term = "";
 
-    table.searchLastName("Zuckerberg");
+        cout << "Actions:" << endl;
+        cout << "1 : print" << endl;
+        cout << "2 : searchFirstName" << endl;
+        cout << "3 : searchLastName" << endl;
+        cout << "4 : searchYear(low, high)" << endl;
 
-    table.searchYear(1973, 2011);
+        cin >> choice;
+
+        if(choice == 1)
+        {
+            table.print();
+        }
+        else if(choice == 2)
+        {
+            cout << "Enter a first name to search by" << endl;
+            cin >> term;
+            table.searchFirstName(term);
+        }
+        else if(choice == 3)
+        {
+            cout << "Enter a last name to search by" << endl;
+            cin >> term;
+            table.searchLastName(term);
+        }
+        else if(choice == 4)
+        {
+            int low = 0;
+            int high = 0;
+
+            cout << "Enter a low year" << endl;
+            cin >> low;
+
+            cout << "Enter a high year" << endl;
+            cin >> high;
+
+            table.searchYear(low, high);
+        }
+        else
+        {
+            cout << "invalid responce" << endl;
+        }
+    }
 }
